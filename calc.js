08e9fdd3eaab125f8ba5774a3a8b0c088d10e877
jeanshotjack <dshalenkova@gmail.com>
2021-11-22 23:10:34 -0500

    completed without readme and comments

---
+++ calc.js
@@ -0,0 +1,118 @@
+// DOM is loaded and ready
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    let yesNo = true;
+
+    // Welcome user to the calculator
+    alert("Welcome to the calculator!");
+
+    // Prompt user for name
+    const name = prompt("Please enter your name: ");
+
+    // Display a message using the user's name
+    document.getElementById("displayName").innerHTML = "Welcome " + name + "!";
+
+    // Instructions on how to use the calculator generate
+    document.getElementById("instructions").innerHTML = "Please enter two comma-separated numbers, then click the button to calculate: ";
+
+    while (yesNo === true) {
+        yesNo = calc();
+
+    }
+
+    // while (inputNums().trim().toLowerCase() === "yes") {
+    //     inputNums();
+    // }
+
+});
+
+// Use a reducer function to add two numbers together 
+function addNums(arr) {
+    let sum = arr.reduce(function (num1, num2) {
+        return num1 + num2;
+    });
+    console.log(sum);
+    return sum;
+}
+
+async function calc() {
+    // Dynamically generate user input as well as a submit button
+    let numInput = document.createElement("INPUT");
+    numInput.type = "text";
+    numInput.placeholder = "e.g. 2,2";
+    numInput.id = "userInput";
+    document.body.appendChild(numInput);
+    let addBtn = document.createElement("button");
+    addBtn.innerText = "Calculate!";
+    document.body.appendChild(addBtn);
+
+    let numSum = 0;
+    let yesNo = true;
+
+    // When the button is clicked, we trim the whitespace, split each on comma, and cast to an integer in an array, and add the nums
+    addBtn.addEventListener("click", function () {
+        let nums = document.getElementById("userInput").value.trim();
+        console.log(nums);
+        let numsArr = nums.split(",").map(function (num) {
+            return parseInt(num);
+        });
+        console.log(numsArr);
+        numSum = addNums(numsArr);
+        yesNo = askAgain(showRes(numSum));
+
+    });
+
+    return yesNo;
+
+}
+
+function showRes(num) {
+
+    let showResult = document.createElement("P");
+    if (num > 10) {
+        showResult.innerText = "The sum of your numbers is: " + num + ". That is a large number!";
+    }
+    else if (num < 10) {
+        showResult.innerText = "The sum of your numbers is: " + num + ". That is a small number!";
+    }
+    else {
+        showResult.innerText = "The sum of your numbers is: " + num + ". That is equal to 10";
+    }
+    document.body.appendChild(showResult);
+
+    const cont = true;
+
+    return cont;
+
+
+}
+
+function askAgain(cont) {
+    let yesNo = true;
+
+    if (cont == true) {
+
+        let ask = document.createElement("P");
+        ask.innerText = "Would you like to try again?";
+        document.body.appendChild(ask);
+        let yesBtn = document.createElement("button");
+        yesBtn.innerText = "Yes";
+        document.body.appendChild(yesBtn);
+        let noBtn = document.createElement("button");
+        noBtn.innerText = "No";
+        document.body.appendChild(noBtn);
+        yesBtn.addEventListener("click", function () {
+            yesNo = true;
+            let newTry = document.createElement("P");
+            newTry.innerText = "Enter two more numbers!";
+            document.body.appendChild(newTry);
+        });
+        noBtn.addEventListener("click", function () {
+            yesNo = false;
+            let stop = document.createElement("P");
+            stop.innerText = "Thank you for using the JavaScript calculator.";
+            document.body.appendChild(stop);
+        });
+    }
+    return yesNo;
+}
